722b602ba77ae0887e1b6bbc673a872141d1f5e1
kuba <kuba@kuba.kuba>
2022-06-28 13:26:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# League of Poro's Esports Capsule Farmer
+
+Are you tired of watching professional League of Legends games? Do you watch only for the drops?
+
+This tool makes the Chrome browser watch the matches for you!
+
+## Features
+- Checks for new live matches
+- Closes finished matches
+
+## Requirements
+- [Chrome browser](https://www.google.com/chrome/)
+
+## How to use
+1. Start the program
+2. Wait for browser to load
+3. Log in
+4. Sit back and relax
+
+## Installation (simple)
+Download and run the latest CapsuleFarmer.exe from Releases tab.
+
+## Installation (advanced)
+
+### Prerequisities
+- Python 3.10
+- pipenv (`pip install pipenv`)
+
+### Step by step
+1. Clone this repo - `git clone https://github.com/LeagueOfPoro/EsportsCapsuleFarmer.git`
+2. `cd EsportsCapsuleFarmer`
+3. Install the Python virtual environment - `pipenv install`
+4. Run the tool - `pipenv run python .\main.py`
+
+## Future features
+- Automatic log in
+- Headless browser (if possible)
---
+++ main.py
@@ -0,0 +1,61 @@
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+import time
+from pprint import pprint
+
+def getLiveMatches(driver):
+    matches = []
+    elements = driver.find_elements(by=By.CSS_SELECTOR, value="a.match.live")
+    for element in elements:
+        matches.append(element.get_attribute("href"))
+    return matches
+
+###################################################
+log = logging.getLogger("Farmer")
+log.setLevel('DEBUG')
+chromedriver_autoinstaller.install()
+
+options = webdriver.ChromeOptions() 
+driver = webdriver.Chrome(options=options)
+driver.get("https://lolesports.com/")
+time.sleep(2)
+
+while not driver.find_elements(by=By.CSS_SELECTOR, value="div.riotbar-summoner-name"):
+    log.info("Waiting for log in")
+    time.sleep(5)
+log.info("Okay, we're in")
+time.sleep(5)
+
+currentWindows = {}
+originalWindow = driver.current_window_handle
+
+while True:
+    liveMatches = getLiveMatches(driver)
+    log.info(f"{len(liveMatches)} matches live")
+
+    # Close windows finished matches
+    toRemove = []
+    for k in currentWindows.keys():
+        if k not in liveMatches:
+            log.info(f"{k} has finished")
+            driver.switch_to.window(currentWindows[k])
+            driver.close()
+            toRemove.append(k)
+            driver.switch_to.window(originalWindow)
+    for k in toRemove:
+        currentWindows.pop(k, None)
+
+    # Open new live matches
+    newLiveMatches = set(liveMatches) - set(currentWindows.keys())
+    log.info(f"{len(newLiveMatches)} new matches")
+    for match in newLiveMatches:
+        driver.switch_to.new_window('tab')
+        driver.get(match)
+        currentWindows[match] = driver.current_window_handle
+    driver.switch_to.window(originalWindow)
+    time.sleep(600)
+
+
+
