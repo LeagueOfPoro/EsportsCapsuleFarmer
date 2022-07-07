42d9e899070996c0a462a811250eaf865156b4b1
kuba <kuba@kuba.kuba>
2022-07-07 09:46:55 +0200

    Support for automatic login

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import chromedriver_autoinstaller
 import time
 from pprint import pprint
@@ -13,7 +15,8 @@ OVERRIDDES = {
     "https://lolesports.com/live/lpl":"https://lolesports.com/live/lpl/lpl",
     "https://lolesports.com/live/lck":"https://lolesports.com/live/lck/lck",
     "https://lolesports.com/live/lec":"https://lolesports.com/live/lec/lec",
-    "https://lolesports.com/live/lcs":"https://lolesports.com/live/lcs/lcs"
+    "https://lolesports.com/live/lcs":"https://lolesports.com/live/lcs/lcs",
+    "https://lolesports.com/live/cblol_academy":"https://lolesports.com/live/cblol_academy/cblol"
 }
 CONFIG_LOCATION="config.yaml"
 
@@ -28,28 +31,50 @@ def readConfig(filepath):
     with open(filepath, "r") as f:
         return yaml.safe_load(f)
 
-# def logIn(driver, username, password):
-#     driver.get("https://lolesports.com/")
-#     time.sleep(2)
-#     pass
+def logIn(driver, username, password):
+    driver.get("https://lolesports.com/")
+    time.sleep(2)
+    els = driver.find_elements(by=By.CSS_SELECTOR, value="a[data-riotbar-link-id=login]")
+    for el in els:
+        try:
+            el.click()
+        except:
+            continue 
+    wait = WebDriverWait(driver, 10)
+    usernameInput = wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "input[name=username]")))
+    usernameInput.send_keys(username)
+    passwordInput = wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "input[name=password]")))
+    passwordInput.send_keys(password)
+    submitButton = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "button[type=submit]")))
+    submitButton.click()
 
-#https://auth.riotgames.com/login
+    # wait until the login process finishes
+    wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "div.riotbar-summoner-name"))) 
+
+
 ###################################################
 log = logging.getLogger("League of Poro")
 log.setLevel('DEBUG')
 chromedriver_autoinstaller.install()
 
 hasValidConfig = False
-username = None
-password = None
+hasAutoLogin = False
+username = "NoUsernameInConfig" # None
+password = "NoPasswordInConfig" #  None
 try:
     config = readConfig(CONFIG_LOCATION)
     hasValidConfig = True
-    pprint(config)
+    if "autologin" in config:
+        if config["autologin"]["enable"]:
+            username = config["autologin"]["username"]
+            password = config["autologin"]["password"]
+            hasAutoLogin = True
 except FileNotFoundError:
-    log.warn("Configuration file not found. IGNORING...")
+    log.warning("Configuration file not found. IGNORING...")
 except (yaml.scanner.ScannerError, yaml.parser.ParserError) as e:
-    log.warn("Invalid configuration file. IGNORING...")
+    log.warning("Invalid configuration file. IGNORING...")
+except KeyError:
+    log.warning("Configuration file is missing mandatory entries. Using default values instead...")
 
 options = webdriver.ChromeOptions() 
 options.add_argument('log-level=3')
@@ -57,8 +82,14 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://lolesports.com/")
 time.sleep(2)
 
+if hasAutoLogin:
+    logIn(driver, username, password)
+
 while not driver.find_elements(by=By.CSS_SELECTOR, value="div.riotbar-summoner-name"):
-    log.info("Waiting for log in")
+    if not hasAutoLogin:
+        log.info("Waiting for log in")
+    else: 
+        log.info("Automatic login failed, please log in manually")
     time.sleep(5)
 log.info("Okay, we're in")
 time.sleep(5)
