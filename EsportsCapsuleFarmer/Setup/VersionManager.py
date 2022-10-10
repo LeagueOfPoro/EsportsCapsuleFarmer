d95365cfb75fa6e4f334b916e41c87d18da5abec
League of Poro <95635582+LeagueOfPoro@users.noreply.github.com>
2022-10-10 10:58:53 +0200

    Check for updates

---
+++ EsportsCapsuleFarmer/Setup/VersionManager.py
@@ -0,0 +1,17 @@
+import requests as req
+
+class VersionManager:
+
+    @staticmethod
+    def getLatestTag():
+        latestTagResponse = req.get("https://api.github.com/repos/LeagueOfPoro/EsportsCapsuleFarmer/releases/latest")
+        if 'application/json' in latestTagResponse.headers.get('Content-Type', ''):
+            latestTagJson = latestTagResponse.json()
+            if "tag_name" in latestTagJson:
+                return float(latestTagJson["tag_name"][1:])
+        return 0.0
+    @staticmethod
+    def isLatestVersion(currentVersion):
+        return currentVersion >= VersionManager.getLatestTag()
+
+print(VersionManager.isLatestVersion(3.8))
